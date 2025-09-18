2c68efe7c48a0d2a8daf2797c6837ce86f81ecdf
1Daisuke <romainmrtns45@gmail.com>
2025-09-18 16:55:09 +0200

    ngtkjfnbjhfr

--- Code/Projet - Don du Sang/Projet - Don du Sang/Questionaires.Designer.cs
+++ Code/Projet - Don du Sang/Projet - Don du Sang/Questionaires.Designer.cs
@@ -45,27 +45,29 @@
             // labNumeroQuestion
             // 
             labNumeroQuestion.AutoSize = true;
-            labNumeroQuestion.Location = new Point(739, 9);
+            labNumeroQuestion.Location = new Point(812, 8);
+            labNumeroQuestion.Margin = new Padding(2, 0, 2, 0);
             labNumeroQuestion.Name = "labNumeroQuestion";
-            labNumeroQuestion.Size = new Size(80, 32);
+            labNumeroQuestion.Size = new Size(44, 17);
             labNumeroQuestion.TabIndex = 0;
             labNumeroQuestion.Text = "* / 10 ";
             // 
             // labQuestionPosee
             // 
-            labQuestionPosee.AutoSize = true;
-            labQuestionPosee.Location = new Point(52, 112);
+            labQuestionPosee.Location = new Point(72, 27);
+            labQuestionPosee.Margin = new Padding(2, 0, 2, 0);
             labQuestionPosee.Name = "labQuestionPosee";
-            labQuestionPosee.Size = new Size(602, 32);
+            labQuestionPosee.Size = new Size(752, 49);
             labQuestionPosee.TabIndex = 1;
             labQuestionPosee.Text = "Question * - *********************************************";
             // 
             // radiobtnOui
             // 
             radiobtnOui.AutoSize = true;
-            radiobtnOui.Location = new Point(13, 51);
+            radiobtnOui.Location = new Point(7, 27);
+            radiobtnOui.Margin = new Padding(2);
             radiobtnOui.Name = "radiobtnOui";
-            radiobtnOui.Size = new Size(83, 36);
+            radiobtnOui.Size = new Size(46, 21);
             radiobtnOui.TabIndex = 2;
             radiobtnOui.TabStop = true;
             radiobtnOui.Text = "Oui";
@@ -75,9 +77,10 @@
             // radiobtnNon
             // 
             radiobtnNon.AutoSize = true;
-            radiobtnNon.Location = new Point(202, 53);
+            radiobtnNon.Location = new Point(348, 27);
+            radiobtnNon.Margin = new Padding(2);
             radiobtnNon.Name = "radiobtnNon";
-            radiobtnNon.Size = new Size(91, 36);
+            radiobtnNon.Size = new Size(51, 21);
             radiobtnNon.TabIndex = 3;
             radiobtnNon.TabStop = true;
             radiobtnNon.Text = "Non";
@@ -87,9 +90,10 @@
             // radiobtnJeNeSaisPas
             // 
             radiobtnJeNeSaisPas.AutoSize = true;
-            radiobtnJeNeSaisPas.Location = new Point(392, 53);
+            radiobtnJeNeSaisPas.Location = new Point(620, 27);
+            radiobtnJeNeSaisPas.Margin = new Padding(2);
             radiobtnJeNeSaisPas.Name = "radiobtnJeNeSaisPas";
-            radiobtnJeNeSaisPas.Size = new Size(189, 36);
+            radiobtnJeNeSaisPas.Size = new Size(107, 21);
             radiobtnJeNeSaisPas.TabIndex = 4;
             radiobtnJeNeSaisPas.TabStop = true;
             radiobtnJeNeSaisPas.Text = "Je ne sais pas";
@@ -101,34 +105,40 @@
             groupBox1.Controls.Add(radiobtnNon);
             groupBox1.Controls.Add(radiobtnOui);
             groupBox1.Controls.Add(radiobtnJeNeSaisPas);
-            groupBox1.Location = new Point(134, 147);
+            groupBox1.Location = new Point(72, 78);
+            groupBox1.Margin = new Padding(2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(585, 113);
+            groupBox1.Padding = new Padding(2);
+            groupBox1.Size = new Size(752, 60);
             groupBox1.TabIndex = 5;
             groupBox1.TabStop = false;
             groupBox1.Text = "groupBox1";
             // 
             // textboxPreciser
             // 
-            textboxPreciser.Location = new Point(146, 266);
+            textboxPreciser.Location = new Point(79, 142);
+            textboxPreciser.Margin = new Padding(2);
+            textboxPreciser.Multiline = true;
             textboxPreciser.Name = "textboxPreciser";
-            textboxPreciser.Size = new Size(624, 39);
+            textboxPreciser.Size = new Size(745, 83);
             textboxPreciser.TabIndex = 7;
             // 
             // labPreciser
             // 
             labPreciser.AutoSize = true;
-            labPreciser.Location = new Point(32, 269);
+            labPreciser.Location = new Point(17, 143);
+            labPreciser.Margin = new Padding(2, 0, 2, 0);
             labPreciser.Name = "labPreciser";
-            labPreciser.Size = new Size(108, 32);
+            labPreciser.Size = new Size(61, 17);
             labPreciser.TabIndex = 6;
             labPreciser.Text = "Préciser :";
             // 
             // btnQuestionPrecedente
             // 
-            btnQuestionPrecedente.Location = new Point(118, 376);
+            btnQuestionPrecedente.Location = new Point(181, 229);
+            btnQuestionPrecedente.Margin = new Padding(2);
             btnQuestionPrecedente.Name = "btnQuestionPrecedente";
-            btnQuestionPrecedente.Size = new Size(257, 46);
+            btnQuestionPrecedente.Size = new Size(138, 24);
             btnQuestionPrecedente.TabIndex = 8;
             btnQuestionPrecedente.Text = "Question précedente";
             btnQuestionPrecedente.UseVisualStyleBackColor = true;
@@ -136,9 +146,10 @@
             // 
             // btnQuestionSuivante
             // 
-            btnQuestionSuivante.Location = new Point(435, 376);
+            btnQuestionSuivante.Location = new Point(584, 229);
+            btnQuestionSuivante.Margin = new Padding(2);
             btnQuestionSuivante.Name = "btnQuestionSuivante";
-            btnQuestionSuivante.Size = new Size(257, 46);
+            btnQuestionSuivante.Size = new Size(138, 24);
             btnQuestionSuivante.TabIndex = 9;
             btnQuestionSuivante.Text = "Question suivante";
             btnQuestionSuivante.UseVisualStyleBackColor = true;
@@ -146,9 +157,10 @@
             // 
             // BtnRetourMenu
             // 
-            BtnRetourMenu.Location = new Point(2, 2);
+            BtnRetourMenu.Location = new Point(1, 1);
+            BtnRetourMenu.Margin = new Padding(2);
             BtnRetourMenu.Name = "BtnRetourMenu";
-            BtnRetourMenu.Size = new Size(150, 46);
+            BtnRetourMenu.Size = new Size(81, 24);
             BtnRetourMenu.TabIndex = 10;
             BtnRetourMenu.Text = "Retour";
             BtnRetourMenu.UseVisualStyleBackColor = true;
@@ -156,9 +168,9 @@
             // 
             // Questionaires
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(831, 493);
+            ClientSize = new Size(867, 262);
             Controls.Add(BtnRetourMenu);
             Controls.Add(btnQuestionSuivante);
             Controls.Add(btnQuestionPrecedente);
@@ -167,6 +179,7 @@
             Controls.Add(groupBox1);
             Controls.Add(labQuestionPosee);
             Controls.Add(labNumeroQuestion);
+            Margin = new Padding(2);
             Name = "Questionaires";
             Text = "Questionaires";
             groupBox1.ResumeLayout(false);
